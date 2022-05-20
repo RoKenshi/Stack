47c607d9dd8da9e6e9f64209b698262ef6674301
RoKenshi <rouronikenshi@gmail.com>
2022-05-20 12:06:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack mystack1 = new Stack();
+        Stack mystack2 = new Stack();
+
+        for(int i = 0; i<10; i++) mystack1.push(i);
+        for(int i = 10; i<20; i++) mystack2.push(i);
+
+        System.out.println("Содержимое стека 1");
+        for(int i = 0; i<10; i++) {
+            System.out.println(mystack1.pop());
+        }
+        System.out.println("Содержимое стека 2");
+        for(int i = 0; i<10; i++) {
+            System.out.println(mystack2.pop());
+        }
+    }
+}
